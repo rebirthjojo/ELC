45436be10b28827aaba2dcbcf21ff019d4c40377
joodan <joodan@naver.com>
2025-11-25 11:18:32 +0900

    메인페이지 수정중

--- src/Pages/main.js
+++ src/Pages/main.js
@@ -1,6 +1,7 @@
-import './main.css'
+import './Main.css'
+import Header from './Header.js'
 
-function main(){
+function Main(){
     return(
         <>
         <Header />
@@ -26,11 +27,22 @@ function main(){
 
             <div className='mainCourseArea'>
                 <div className='courseTitle'>인기강의</div>
-                <div className='courseDetail'>
-                    <div className=''></div>
+                <div className='courseContent'>
+                    <div className='courseImage'></div>
+                    <div className='courseDetail'></div>
                 </div>
-
             </div>
+
+            <div className='adArea'>
+                <div className='adAreaTitle'>신뢰받는 학습 플랫폼</div>
+                <div className='adAreaSubTitle'>수많은 학습자들이 E-Learming과 함께 성장하고 있습니다</div>
+                <div className='adAreaContent'>
+                    <div className='adAreaIcon'></div>
+                    <div className='adAreaText1'>500,000+</div>
+                    <div className='adAreaText2'>활동 중인 수강생</div>
+                </div>
+            </div>
+            
         </Main>
         
         <Footer />
@@ -38,4 +50,4 @@ function main(){
     );
 }
 
-export default main;
+export default Main;
