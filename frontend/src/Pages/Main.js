4d50bd9a1ce851ec9a1459a1ed70d9cb0229a156
rebirthjojo <aiuy05@gmail.com>
2026-01-09 14:50:50 +0900

    결제 페이지 수정

--- frontend/src/Pages/Main.js
+++ frontend/src/Pages/Main.js
@@ -21,7 +21,7 @@ const Main = ()=>{
         navigate(`/Detail/${encodeURIComponent(lectureTitle)}`);
     };
 
-    const handleCheckClick =()=>{
+    const handleCheckClick =(lectureTitle)=>{
         navigate(`/Checkout/${encodeURIComponent(lectureTitle)}`);
     };
     
